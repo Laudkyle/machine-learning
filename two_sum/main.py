96fef8a5a0a497ce42500b3e6148812de80ea066
Laudkyle <86990808+Laudkyle@users.noreply.github.com>
2022-07-08 05:29:42 +0000

    Delete two_sum directory

--- two_sum/main.py
+++
@@ -1,9 +0,0 @@
-list1=[1,2,3,4,5,6,7,8,9,10]
-
-def two_sum(list, target):
-    for i in list:
-        for ii in list:
-            if (int(i) + int(ii) == int(target)):
-                print(f"{list.index(i)},{list.index(ii)}")
-    return 0
-two_sum(list1,12)
